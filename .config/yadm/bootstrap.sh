a56bd2e7298bf6f730a25b2b475f6800cd7be10b
Francesco Scheffczyk <francesco.scheffczyk@gmail.com>
2024-02-13 15:54:48 +0100

    Work on bootstrap file to install Homebrew and Oh my ZSH

--- .config/yadm/bootstrap.sh
+++ .config/yadm/bootstrap.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
 
-echo "Setting up your Mac..."
-return
+########## 
+# Mac OS # 
+##########
+if [ "$(uname)" = "Darwin" ]
+then 
+    echo "Running on Mac OS"
+
+    # Install Homebrew
+    echo "Installing Homebrew..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+    # Install Oh My Zsh
+    echo "Installing Oh My Zsh..."
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
