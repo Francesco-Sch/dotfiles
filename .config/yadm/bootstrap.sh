d3e7d6e9754066f5cbbd964f50d20d51c24b9ab7
Francesco Scheffczyk <francesco.scheffczyk@gmail.com>
2024-12-21 15:04:37 +0100

    chore: Add checks to bootstrap file to see if certain programs are already installed

--- .config/yadm/bootstrap.sh
+++ .config/yadm/bootstrap.sh
@@ -1,17 +1,32 @@
 #!/bin/sh
 
+# Get the system type
+# system type is an environment variable that stores the current operating system
+# i.e. Darwin (Mac OS), Ubuntu (Linux), etc.
+system_type=$(uname -s)
+
 ########## 
 # Mac OS # 
 ##########
-if [ "$(uname)" = "Darwin" ]
+if [ "$system_type" = "Darwin" ]
 then 
     echo "Running on Mac OS"
 
     # Install Homebrew
-    echo "Installing Homebrew..."
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    if ! command -v brew &> /dev/null
+    then
+        echo "Installing Homebrew..."
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    else
+        echo "Homebrew is already installed."
+    fi
 
     # Install Oh My Zsh
-    echo "Installing Oh My Zsh..."
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    if ! command -v zsh &> /dev/null
+    then
+        echo "Installing Oh My Zsh..."
+        sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    else
+        echo "Oh My Zsh is already installed."
+    fi
 fi
