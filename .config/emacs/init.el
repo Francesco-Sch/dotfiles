5bf5500eb1caccaf1451a48bd41b48061cce1d32
Francesco Scheffczyk <francesco.scheffczyk@gmail.com>
2025-12-11 18:40:10 +0100

    feat(emacs): Add Vertico and savehist-mode to emacs configuration

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -14,3 +14,23 @@
 
 (add-hook 'org-mode-hook #'org-indent-mode) ; Indent text below headlines
 (add-hook 'org-mode-hook #'visual-line-mode) ; Wraps lines that are longer then the window
+
+(use-package vertico
+  :ensure t
+  :init (vertico-mode))
+
+(use-package savehist
+  :ensure nil ; Built-in
+  :init (savehist-mode))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
